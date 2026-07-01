60f88b332ea08170a1a46210aca38ab021f39df7
ROLI1631 <founder@so8fi.info>
2026-07-01 17:52:01 +0000

    Configure Cloudflare deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 export default nextConfig;
+
+if (process.env.NODE_ENV === 'development') {
+  await initOpenNextCloudflareForDev();
+}
